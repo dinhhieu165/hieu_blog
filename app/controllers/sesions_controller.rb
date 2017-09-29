653acede9efebce16a4f901268b3164d155f119f
Nguyen Dinh Hieu <dinhhieu16598@gmail.com>
2017-09-29 19:10:35 +0700

    Implement basic login

---
+++ app/controllers/sesions_controller.rb
@@ -0,0 +1,21 @@
+class SesionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+      flash[:success] = "Welcome to the Sample App!"
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  get 'sesions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  '/signup',  to: 'static_pages#signup'
-  get  '/login',   to: 'static_pages#login'
+  get  '/signup',  to: 'users#signup'
+  get  '/login',   to: 'sesions#new'
+  post   '/login',   to: 'sesions#create'
+  delete '/logout',  to: 'sesions#destroy'
   resources :users
 end
