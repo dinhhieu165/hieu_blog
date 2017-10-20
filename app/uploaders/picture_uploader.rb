f3927bf21602e3389b38878358ddd14002ddf503
Nguyen Dinh Hieu <dinhhieu16598@gmail.com>
2017-10-20 20:57:20 +0700

    delete EVN

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,11 +8,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
 
-  if Rails.env.production?
-    storage :fog
-  else
+  # if Rails.env.production?
+  #   storage :fog
+  # else
     storage :file
-  end
+  # end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
